4ac704400b00d7f06057e3941b7bcdc1a21f88d1
Ariel <arielplante02@gmail.com>
2024-09-02 21:37:27 -0300

    Se cargaron archivos nuevos

---
+++ Server.js
@@ -0,0 +1,4 @@
+import app from "./app";
+
+
+app.listen(8080,()=> console.log("Escucha en puerto 8080"));
---
+++ app.test.js
@@ -0,0 +1,32 @@
+import supertest from "supertest";
+import app from "./app";
+
+describe ( "post/users" , () => 
+{
+    describe ( "Usuario y clave son correctos" , () =>
+        {
+            /*
+                Deberia ser un objeto JSON
+                Deberia recibir un status 200
+                Debe guardar en BD
+                Debe tener JSON en el header
+            */
+        });
+
+        describe ( "Usuario y clave incorrectos" , () =>
+            {
+                /*
+                   Deberia responder un status 400
+                */
+            });
+
+            test( "Deberia ser content-type igaul a JSON" , async () =>
+                {
+                    const response = await supertest(app).post("/users").send(
+                        {
+                            username = "username",
+                            password = "password"
+                        });
+                        expect (response.headers["content-type"]).toEqual(except.stringContaining("Json"));
+                });
+});
